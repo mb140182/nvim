4d95b006db4c7520c5a47870deecb7e75c2361d7
mb14 <mb140182@gmail.com>
2025-05-29 09:00:45 +0300

    Add BufExplorer - 2

--- lua/plugins/bufexplorer.lua
+++ lua/plugins/bufexplorer.lua
@@ -8,19 +8,5 @@ return {
 ]]
 
       --vim.keymap.set('n', '<leader>tq', "<cmd>BufExplorer<cr>", { desc = '[B]ufExplorer [W]ord' }),  --rabotaet
---Dla primera
-      --vim.keymap.set('n', '<leader>tq', "<cmd>BufferListOpen<cr>", { desc = '[S]earch current [W]ord' }),  --rabotaet
-
-    --keys = {
-      --{
-        --'<leader>f',
-        --function()
-          --require('conform').format { async = true, lsp_format = 'fallback' }
-        --end,
-        --mode = '',
-        --desc = '[F]ormat buffer',
-      --},
-    --},
-
   }
 }
